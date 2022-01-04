c66e185bffde893cb47d53e8557efa9ef26282c0
n3316202 <nqwrt@naver.com>
2022-01-04 16:48:37 +0900

    2022-01-04 시큐리티 6단계 까지

--- src/main/java/edu/kosmo/ex/controller/SecurityController.java
+++ src/main/java/edu/kosmo/ex/controller/SecurityController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.log4j.Log4j;
+
+@Log4j
 @RequestMapping("/security/*")
 @Controller
 public class SecurityController {
@@ -30,5 +33,24 @@ public class SecurityController {
 		System.out.println("logined member");
 		return "/security/member";
 	}
+	
+	@GetMapping("/admin")
+	public void admin() {
+	
+		log.info("/admin..");
+		
+		//return "security/admin"
+	}
+	
+	@GetMapping("/accessError")
+	public void accessError(Model model) {
+	
+		log.info("/accessError()..");
+		
+		model.addAttribute("msg","ACCESS Denied(403 에러)");	
+		
+	}
+	
+	
 	
 }
